ea2347b347807c1a8cefa2c16ad50ebf7dbcbaf4
Pedro <pedro@voffice.com.br>
2021-09-21 12:57:16 +0100

    feat: add PInputNumber

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './button'
 export * from './form/PListbox'
 export { default as PInput } from './form/PInput'
+export { default as PInputNumber } from './form/PInputNumber'
 export { default as PListBox } from './form/PListbox'
 export { default as PSelect } from './form/PSelect'
