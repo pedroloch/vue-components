92f95e8e0f652f7dc6c1830371c2cb1fdad94833
Pedro <pedro@voffice.com.br>
2021-09-23 17:34:34 +0100

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      // external: ['vue', '@headlessui/vue', 'maska'],
+      external: ['vue', '@headlessui/vue', 'maska', 'lodash'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
